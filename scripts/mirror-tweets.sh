fb8d947345163849bb11c2dffd8c87a93b6c8512
James Taylor <jt-nti@users.noreply.github.com>
2020-06-27 12:07:06 +0100

    Update mirror-tweets.sh

--- scripts/mirror-tweets.sh
+++ scripts/mirror-tweets.sh
@@ -5,6 +5,10 @@ set -ev
 SINCE=$(date --date='yesterday' +%F)
 UNTIL=$(date +%F)
 
-twurl -j "/1.1/search/tweets.json?q=(from:pauljholmes)+since:${SINCE}+until:${UNTIL}&result_type=recent&tweet_mode=extended" | jq --raw-output '.statuses[].full_text' | sed 's/\B@\([a-zA-Z0-9_]\+\)/\1/g' | cut -c 1-280 > tweets.txt
+twurl -j "/1.1/search/tweets.json?q=(from:pauljholmes)+since:${SINCE}+until:${UNTIL}&result_type=recent&tweet_mode=extended" | \
+  jq --raw-output '.statuses[].full_text' | \
+  sed 's/\B@\([a-zA-Z0-9_]\+\)/\1/g' | \
+  cut -c 1-280 | \
+  tac > tweets.txt
 
 cat tweets.txt
