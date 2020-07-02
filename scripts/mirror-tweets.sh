2ddb7508be5173f6dd80d0f13c143df71324d0f6
James Taylor <jt-nti@users.noreply.github.com>
2020-07-02 17:53:25 +0100

    Update mirror-tweets.sh

--- scripts/mirror-tweets.sh
+++ scripts/mirror-tweets.sh
@@ -10,13 +10,20 @@ twurl "/1.1/search/tweets.json?q=(from:pauljholmes)+since:${SINCE}+until:${UNTIL
   tac | \
   split -l 1 - tweet
 
-if [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
-  echo "${TRAVIS_EVENT_TYPE}: Tweet here!"
+if [ -f tweetaa ]; then
+  if [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
+    echo "${TRAVIS_EVENT_TYPE}: Tweet here!"
+    find . -type f -name 'tweet*' -print0 | \
+      sort -z | \
+      xargs -0 -L 1 sh -c 'file="$1"; tweet=$(cat "$file" | jq -r . | sed "s/\B@\([a-zA-Z0-9_]\+\)/\1/g" | cut -c 1-280); echo "Tweet: $tweet"' _
+  else
+    echo "${TRAVIS_EVENT_TYPE}: Don't tweet!"
+    find . -type f -name 'tweet*' -print0 | \
+      sort -z | \
+      xargs -0 -L 1 sh -c 'file="$1"; tweet=$(cat "$file" | jq -r . | sed "s/\B@\([a-zA-Z0-9_]\+\)/\1/g" | cut -c 1-280); echo "Tweet: $tweet"' _
+  fi
 else
-  echo "${TRAVIS_EVENT_TYPE}: Don't tweet!"
-  find . -type f -name 'tweet*' -print0 | \
-    sort -z | \
-    xargs -0 -L 1 sh -c 'file="$1"; tweet=$(cat "$file" | jq -r . | sed "s/\B@\([a-zA-Z0-9_]\+\)/\1/g" | cut -c 1-280); echo "Tweet: $tweet"' _
+  echo "${TRAVIS_EVENT_TYPE}: No tweets!"
 fi
 
 echo Done
