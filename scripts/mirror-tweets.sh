c07772153d2d4883c90ddef6844caa8d6006db69
James Taylor <jt-nti@users.noreply.github.com>
2020-07-02 17:36:32 +0100

    Update mirror-tweets.sh

--- scripts/mirror-tweets.sh
+++ scripts/mirror-tweets.sh
@@ -5,25 +5,18 @@ set -ev
 SINCE=$(date --date='yesterday' +%F)
 UNTIL=$(date +%F)
 
-twurl -j "/1.1/search/tweets.json?q=(from:pauljholmes)+since:${SINCE}+until:${UNTIL}&result_type=recent&tweet_mode=extended" | \
-  jq --raw-output '.statuses[].full_text' | \
-  sed 's/\B@\([a-zA-Z0-9_]\+\)/\1/g' | \
-  cut -c 1-280 | \
-  tac > tweets.txt
+twurl "/1.1/search/tweets.json?q=(from:pauljholmes)+since:${SINCE}+until:${UNTIL}&result_type=recent&tweet_mode=extended" | \
+  jq -c '.statuses[].full_text' | \
+  tac | \
+  split -l 1 - tweet
 
-MAX_TWEET_LEN=$(wc -L < tweets.txt)
-
-if [ ${MAX_TWEET_LEN:-0} -gt 0 ]; then
-  cat tweets.txt
-fi
-
-if [ ${TRAVIS_EVENT_TYPE} = "cron" ] && [ ${MAX_TWEET_LEN:-0} -gt 0 ]; then
+if [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
   echo "${TRAVIS_EVENT_TYPE}: Tweet here!"
-  xargs -0 -d '\n' -L 1 -I{} echo Tweet: {} < tweets.txt
-  # xargs -0 -d '\n' -L 1 -I{} twurl -d 'status={}' /1.1/statuses/update.json < tweets.txt
 else
-  echo "${TRAVIS_EVENT_TYPE}: Don't tweet! ${MAX_TWEET_LEN}"
-  xargs -0 -d '\n' -L 1 -I{} echo Tweet: {} < tweets.txt
+  echo "${TRAVIS_EVENT_TYPE}: Don't tweet!"
+  find . -type f -name 'tweet*' -print0 | \
+    sort -z | \
+    xargs -0 -L 1 sh -c 'file="$1"; tweet=$(cat "$file" | jq -r . | sed "s/\B@\([a-zA-Z0-9_]\+\)/\1/g" | cut -c 1-280); echo "Tweet: $tweet"' _
 fi
 
 echo Done
